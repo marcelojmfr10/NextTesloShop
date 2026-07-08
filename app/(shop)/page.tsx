9132965da0ec3562a5dca9fa195eeebfbbb2424c
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-07-08 11:47:56 -0600

    Componente grid de productos

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,4 +1,7 @@
-import { Title } from "../components";
+import { ProductGrid, Title } from "../components";
+import { initialData } from "../seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
@@ -8,6 +11,8 @@ export default function Home() {
         subTitle="Todos los productos"
         className="mb-2"
       ></Title>
+
+      <ProductGrid products={products} />
     </>
   );
 }
