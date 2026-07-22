389abbf9649170512199468cf91f29a785d2d901
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-07-21 20:13:36 -0600

    Modelo de orden

--- app/seed/seed-database.ts
+++ app/seed/seed-database.ts
@@ -4,6 +4,9 @@ import { countries } from "./seed-countries.ts";
 
 async function main() {
   // await Promise.all([
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
   await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
   await prisma.country.deleteMany();
